ba8203bfe3e5aa493564c6a61508deaf4a565b82
Jieun Kang <ochoby@gmail.com>
2024-03-01 20:43:15 +0900

    송금 input 컴포넌트 작성 완료

--- src/components/auth/AccountPW.tsx
+++ src/components/auth/AccountPW.tsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import { AccountPwValid } from '../../business/valid/AccountPwValid';
+import { KeyPad } from '../common/KeyPad';
 import { ChildrenValue } from '../../types/Types';
 
 type PropsType = {
@@ -7,19 +8,38 @@ type PropsType = {
     width?: string;
 };
 
-
-
-
 export const AccountPW = ({ onAccountPWChange, width }: PropsType): JSX.Element => {
-    const [accountPassword, setAccountPassword] = useState<string>("")
+    const [isKeyPadOpen, setIsKeyPadOpen] = useState(false);
+    const [accountPassword, setAccountPassword] = useState<string>("");
+    const inputRef = useRef<HTMLInputElement>(null);
+    const MAX_ACCOUNTPW_LENGTH = 6;
 
-    const handlePrePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const pw = event.target.value.replace(/\D/g, '').slice(0, 6);
-        setAccountPassword(pw)
-        onAccountPWChange({ value: pw, valid: AccountPwValid(accountPassword) })
+    useEffect(() => {
+        // 입력이 6자리가 되면 KeyPad 닫기
+        if (accountPassword.length === MAX_ACCOUNTPW_LENGTH) {
+            setIsKeyPadOpen(false);
+        }
+    }, [accountPassword]);
 
+    const handlePrePasswordChange = (num: string) => {
+        if (MAX_ACCOUNTPW_LENGTH < num.length) return;
+
+        setAccountPassword(num);
+        onAccountPWChange({ value: num, valid: AccountPwValid(num) });
+
+        // 입력이 6자리가 되면 KeyPad 닫기
+        if (num.length === MAX_ACCOUNTPW_LENGTH) {
+            setIsKeyPadOpen(false);
+        }
     };
 
+    const handleInputClick = () => {
+        setIsKeyPadOpen(true);
+        // 6자리가 입력된 상태에서 다시 input 클릭하면 KeyPad 열기
+        if (accountPassword.length === MAX_ACCOUNTPW_LENGTH) {
+            setAccountPassword("");
+        }
+    };
 
     return (
         <>
@@ -28,17 +48,16 @@ export const AccountPW = ({ onAccountPWChange, width }: PropsType): JSX.Element 
                     <span className="label-text">계좌 비밀번호</span>
                 </label>
                 <input
+                    ref={inputRef}
                     type="password"
                     placeholder="숫자 6자리로 입력해주세요."
                     className={`input input-bordered input-primary ${width}`}
                     value={accountPassword}
-                    onChange={handlePrePasswordChange}
+                    onClick={handleInputClick}
+                    readOnly
                 />
+                {isKeyPadOpen && <KeyPad onPWChange={handlePrePasswordChange} />}
             </div>
-
-
-
         </>
     );
 };
-
