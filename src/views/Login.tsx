289b2a3d49e31cf8583d635a32201eb8595889d2
Jieun Kang <ochoby@gmail.com>
2024-04-04 23:15:52 +0900

    구글로그인버튼 삭제

--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { Link } from 'react-router-dom'
 import { Password } from '../components/auth/Password';
 import { BtnLogin } from '../components/auth/BtnLogin';
-import { BtnGoogleLogin } from '../components/auth/BtnGoogleLogin';
+// import { BtnGoogleLogin } from '../components/auth/BtnGoogleLogin';
 import { Email } from '../components/auth/Email';
 import type { ChildrenValue } from '../types/Types';
 
@@ -23,7 +23,7 @@ const Login = (): JSX.Element => {
     <>
       <div className="container min-h-screen">
         <div className="flex flex-col items-center">
-          <div className="text-center">
+          <div className="text-center mt-24">
             <Link to="/">
               <h1 className="text-5xl font-bold">Tikul</h1>
             </Link>
@@ -36,12 +36,12 @@ const Login = (): JSX.Element => {
                 <Password onValueChange={handlePasswordChange} />
               </form>
               <BtnLogin email={email.value} password={password.value} />
-              <div className="flex items-center justify-around">
+              {/* <div className="flex items-center justify-around">
                 <hr className="w-1/3"></hr>
                 <span>or</span>
                 <hr className="w-1/3"></hr>
-              </div>
-              <BtnGoogleLogin />
+              </div> */}
+              {/* <BtnGoogleLogin /> */}
             </div>
           </div>
         </div>
